514c4c123fa26076a43e8177210675a08f0dce19
Berkay Zaim <88087972+berkayzaimdev@users.noreply.github.com>
2023-01-27 21:20:17 +0300

    Create 02_Java Exception Handling.java

---
+++ Java/Exception Handling/02_Java Exception Handling.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+class MyCalculator 
+{
+    /*  * Create the method long power(int, int) here.
+    */
+    long power(int x,int y) throws Exception
+    {
+        long sum=1;
+        if(x==0&&y==0)
+        throw new Exception("n and p should not be zero.");
+        else if(x<0||y<0)
+        throw new Exception("n or p should not be negative.");
+        for(int i=0;i<y;i++)
+        {
+            sum*=x;
+        }
+        return sum;
+    }
+}
+
+public class Solution {
+    public static final MyCalculator my_calculator = new MyCalculator();
+    public static final Scanner in = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        while (in .hasNextInt()) {
+            int n = in .nextInt();
+            int p = in .nextInt();
+            
+            try {
+                System.out.println(my_calculator.power(n, p));
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
