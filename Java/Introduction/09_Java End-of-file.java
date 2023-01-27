8704c71db9c1c81d17913d47cc69db4b078f09e8
Berkay Zaim <88087972+berkayzaimdev@users.noreply.github.com>
2023-01-27 20:31:19 +0300

    Create 09_Java End-of-file.java

---
+++ Java/Introduction/09_Java End-of-file.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution 
+{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int i = 0 ;
+        while(s.hasNext())
+        {
+            System.out.println((++i)+" "+s.nextLine());
+        }
+    }
+}
