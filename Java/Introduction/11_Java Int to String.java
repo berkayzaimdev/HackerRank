cc89d2c903488fb44b26fec74b59eee9e5756def
Berkay Zaim <88087972+berkayzaimdev@users.noreply.github.com>
2023-01-27 20:35:04 +0300

    Create 11_Java Int to String.java

---
+++ Java/Introduction/11_Java Int to String.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution 
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try
+        {
+            int n = sc.nextInt();
+            String s = Integer.toString(n);
+            System.out.println("Good job");
+        }
+        catch(Exception e)
+        {
+            System.out.println("Wrong answer");
+        }
+    }
+}
