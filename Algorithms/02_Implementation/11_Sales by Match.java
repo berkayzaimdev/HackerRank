b3468425f41c6378e445d4e59c55bf7a06c9a531
Berkay Zaim <88087972+berkayzaimdev@users.noreply.github.com>
2023-02-10 19:57:52 +0300

    Create 11_Sales by Match.java

---
+++ Algorithms/02_Implementation/11_Sales by Match.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result 
+{
+    public static int sockMerchant(int n, List<Integer> ar)
+    {
+        int pairs = 0;
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i : ar)
+            map.put(i,map.containsKey(i)?map.get(i)+1:1);
+        for(int val : map.values())
+        pairs+=val/2;
+        return pairs;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
