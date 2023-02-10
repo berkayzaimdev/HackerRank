ce4f22afd510dc2f743d4be15bdfd7b47e22486a
Berkay Zaim <88087972+berkayzaimdev@users.noreply.github.com>
2023-02-10 22:27:18 +0300

    Create 13_Counting Valleys.java

---
+++ Algorithms/02_Implementation/13_Counting Valleys.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result 
+{
+    public static int countingValleys(int steps, String path)
+    {
+        int sum=0,c=0;
+        for(char i : path.toCharArray())
+        {
+            sum = sum + (i == 'U'? 1 : -1);
+            if(sum==0&&i=='U')
+            c++;
+        }
+        return c;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int steps = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String path = bufferedReader.readLine();
+
+        int result = Result.countingValleys(steps, path);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
