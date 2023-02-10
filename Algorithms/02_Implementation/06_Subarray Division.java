8231ddebcda2872438ef600dc52d94e821ef9ae3
Berkay Zaim <88087972+berkayzaimdev@users.noreply.github.com>
2023-02-10 19:56:17 +0300

    Create 06_Subarray Division.java

---
+++ Algorithms/02_Implementation/06_Subarray Division.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result 
+{
+    public static int birthday(List<Integer> s, int d, int m)
+    {
+        int c=0,sum,j;
+        for(int i=0;i<s.size();i++)
+        {
+            sum=0;
+            j=0;
+            while(i+j<s.size()&&j<m)
+            {
+                sum+=s.get(i+(j++));
+            }
+            if(sum==d)
+            c++;
+        }
+        return c;   
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> s = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int d = Integer.parseInt(firstMultipleInput[0]);
+
+        int m = Integer.parseInt(firstMultipleInput[1]);
+
+        int result = Result.birthday(s, d, m);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
