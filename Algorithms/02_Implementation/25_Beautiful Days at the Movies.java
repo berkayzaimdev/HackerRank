699a2dd7ab1a328fc6a09bc5d214470dfaeffda5
Berkay Zaim <88087972+berkayzaimdev@users.noreply.github.com>
2023-02-13 04:56:40 +0300

    Create 25_Beautiful Days at the Movies.java

---
+++ Algorithms/02_Implementation/25_Beautiful Days at the Movies.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result 
+{
+    public static int beautifulDays(int i, int j, int k)
+    {
+        int sday = 0;
+        for(i=i;i<=j;i++)
+        if((i-reverse(i))%k==0) sday++;
+        return sday;
+    }
+    
+    private static int reverse(int n)
+    {
+        int r = 0;
+        while(n!=0)
+        {
+            r = r*10 + n%10;
+            n /= 10;
+        }
+        return n+r;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int i = Integer.parseInt(firstMultipleInput[0]);
+
+        int j = Integer.parseInt(firstMultipleInput[1]);
+
+        int k = Integer.parseInt(firstMultipleInput[2]);
+
+        int result = Result.beautifulDays(i, j, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
