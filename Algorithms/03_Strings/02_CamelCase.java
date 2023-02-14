fdc06ce7e67f613f7e556a20497d88c33f944503
Berkay Zaim <88087972+berkayzaimdev@users.noreply.github.com>
2023-02-14 12:36:02 +0300

    Create 02_CamelCase.java

---
+++ Algorithms/03_Strings/02_CamelCase.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result 
+{
+    public static int camelcase(String s)
+    {
+        int result=1;
+        for(char c : s.toCharArray())
+        if(c==Character.toUpperCase(c)) result++;
+        return result;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        int result = Result.camelcase(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
