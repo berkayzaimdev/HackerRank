9e3ae070812dcc510f44160c47b1741c7205fbb7
Berkay Zaim <88087972+berkayzaimdev@users.noreply.github.com>
2023-02-14 03:18:06 +0300

    Create 31_Find Digits.java

---
+++ Algorithms/02_Implementation/31_Find Digits.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result 
+{
+    public static int findDigits(int n) 
+    {
+        int c=0;
+        int temp=n;
+        while(temp>0) 
+        {
+            if (temp % 10 != 0 && n % (temp % 10) == 0)
+            c++;
+            temp /= 10;
+        }
+        return c;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int result = Result.findDigits(n);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
